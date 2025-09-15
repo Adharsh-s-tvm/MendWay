6c09b779b705b2b6a6caf614c36b756d2ac43272
Adharsh S <adharshkattaikonam@gmail.com>
2025-09-15 19:05:37 +0530

    Project initially committed

---
+++ backend/src/server.ts
@@ -0,0 +1,5 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+app.get('/health', (_, res) => res.send('OK'));
+app.listen(3001, () => console.log('Backend running'))
