0ffb55ac1882a52e7df86c1420ef3b5fc7ad4b75
Adharsh S <adharshkattaikonam@gmail.com>
2025-09-21 22:43:51 +0530

    Frontend sample page initalized

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <>
-    
-    </>
+    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gray-800">Hello, Next.js 🚀</h1>
+        <p className="mt-4 text-gray-600">
+          This is a simple page without extra components.
+        </p>
+      </div>
+    </main>
   );
 }
