e89ebb5d956d29856b07c9491f8426f1db7281d1
Adharsh S <adharshkattaikonam@gmail.com>
2025-10-01 22:25:36 +0530

    Server running...

--- backend/src/infrastructure/database/connection.ts
+++ backend/src/infrastructure/database/connection.ts
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 export async function connectDB(uri: string) {
     try {
         await mongoose.connect(uri);
-        console.log("✅ MongoDB Connected");
+        console.log("MongoDB Connected");
     } catch (err) {
-        console.error("❌ Database connection failed", err);
+        console.error("Database connection failed", err);
         process.exit(1);
     }
 }
--- backend/src/infrastructure/utils/UUIDGenerator.ts
+++ backend/src/infrastructure/utils/UUIDGenerator.ts
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from "uuid";
 import { IGenerateUserID } from "../../application/services/IGenerateUserID";
 
 export class UUIDGenerator implements IGenerateUserID {
-  async create(): Promise<string> {
-    return uuidv4();
-  }
+    async create(): Promise<string> {
+        return uuidv4();
+    }
 }
