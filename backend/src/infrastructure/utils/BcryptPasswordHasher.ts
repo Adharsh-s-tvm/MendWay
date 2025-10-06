1cddb9dadebd00c2a04211bb1dc0ee719782b5f1
Adharsh S <adharshkattaikonam@gmail.com>
2025-10-06 12:40:10 +0530

    Both backend and frontend server running successfully. The backend package.json file name changed. removed src baseurl from frontend. Some red lines in tsconfig both frontend and backend...

--- backend/src/infrastructure/utils/BcryptPasswordHasher.ts
+++ backend/src/infrastructure/utils/BcryptPasswordHasher.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { IPasswordHasher } from "../../application/services/IPasswordHasher";
 
 export class BcryptPasswordHasher implements IPasswordHasher {
